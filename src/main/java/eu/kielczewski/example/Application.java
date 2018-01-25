04723078b44ac5d7c91a75d650e14dd22e72e05b
srinivas <srinivas.c1992@gmailc.com>
2018-01-25 20:43:49 +0530

    llll

--- src/main/java/eu/kielczewski/example/Application.java
+++ src/main/java/eu/kielczewski/example/Application.java
@@ -11,7 +11,6 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
